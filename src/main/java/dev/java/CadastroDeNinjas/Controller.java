7c55b434aab60bd93c1fae56f668174052f77e0c
Gabriel Martins <gamartins77@gmail.com>
2025-09-07 18:05:51 -0300

    Controller criado aula 2

---
+++ src/main/java/dev/java/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package dev.java.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Seja bem vindo a primeira rota criada !";
+    }
+
+}
