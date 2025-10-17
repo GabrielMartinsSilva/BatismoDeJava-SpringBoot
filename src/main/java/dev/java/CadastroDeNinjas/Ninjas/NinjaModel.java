3ff8e44699033f86359f47782586559977488332
Gabriel Martins <gamartins77@gmail.com>
2025-10-17 19:26:15 -0300

    Feat: adicionado Lombok ao projeto

--- src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,18 @@ package dev.java.CadastroDeNinjas.Ninjas;
 
 import dev.java.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -26,36 +33,5 @@ public class NinjaModel {
     @JoinColumn (name = "missoes_id" )
     private List<MissoesModel> missoes;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
